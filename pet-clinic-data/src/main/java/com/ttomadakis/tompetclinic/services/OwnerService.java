9cabb50701ae47693959dd922a11ea136626097c
tomadakis <tomadakis@oktopay.eu>
2021-10-05 17:05:01 +0300

    Refactoring to common crud interface. Closes #11.

--- pet-clinic-data/src/main/java/com/ttomadakis/tompetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/ttomadakis/tompetclinic/services/OwnerService.java
@@ -2,15 +2,7 @@ package com.ttomadakis.tompetclinic.services;
 
 import com.ttomadakis.tompetclinic.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long>{
 
     Owner findByLastName(String lastName);
-
-    Owner findById(Long id);
-
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
 }
--- pet-clinic-data/src/main/java/com/ttomadakis/tompetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/ttomadakis/tompetclinic/services/VetService.java
@@ -2,13 +2,6 @@ package com.ttomadakis.tompetclinic.services;
 
 import com.ttomadakis.tompetclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
