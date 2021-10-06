e048a0af80e5db1799d336e2675ab3a4bf273df1
tomadakis <tomadakis@oktopay.eu>
2021-10-06 14:59:38 +0300

    Implemented Map based Services. Closes #6.

---
+++ pet-clinic-data/src/main/java/com/ttomadakis/tompetclinic/services/map/OwnerServiceMap.java
@@ -0,0 +1,34 @@
+package com.ttomadakis.tompetclinic.services.map;
+
+import com.ttomadakis.tompetclinic.model.Owner;
+import com.ttomadakis.tompetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Owner save(Owner owner) {
+        return super.save(owner.getId(), owner);
+    }
+
+    @Override
+    public void delete(Owner owner) {
+        super.delete(owner);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
---
+++ pet-clinic-data/src/main/java/com/ttomadakis/tompetclinic/services/map/PetServiceMap.java
@@ -0,0 +1,34 @@
+package com.ttomadakis.tompetclinic.services.map;
+
+import com.ttomadakis.tompetclinic.model.Pet;
+import com.ttomadakis.tompetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet pet) {
+        return super.save(pet.getId(), pet);
+    }
+
+    @Override
+    public void delete(Pet pet) {
+        super.delete(pet);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
