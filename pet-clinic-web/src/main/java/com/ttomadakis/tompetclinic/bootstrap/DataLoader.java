3dfc0c3f23ddb828f76c1e48626b69ac5323a4c3
tomadakis <tomadakis@oktopay.eu>
2021-10-20 17:18:59 +0300

    Added List of Owners on Owner Index Page. Closes #17.

--- pet-clinic-web/src/main/java/com/ttomadakis/tompetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/ttomadakis/tompetclinic/bootstrap/DataLoader.java
@@ -28,9 +28,9 @@ public class DataLoader implements CommandLineRunner {
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner1.setId(2L);
-        owner1.setFirstName("Fiona");
-        owner1.setLastName("Glennane");
+        owner2.setId(2L);
+        owner2.setFirstName("Fiona");
+        owner2.setLastName("Glennane");
 
         ownerService.save(owner2);
 
